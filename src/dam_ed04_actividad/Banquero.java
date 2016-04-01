c6ede5d394a683eecaefb99fa1034965c49e7ac8
SamuelRuGo <samuel.skorp21@gmail.com>
2016-04-01 18:24:50 +0100

    Commit 6

---
+++ src/dam_ed04_actividad/Banquero.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dam_ed04_actividad;
+
+/**
+ *
+ * @author Samuel
+ */
+public class Banquero {
+    private String dni;
+    private String nombre;
+    private String apellidos;
+}
